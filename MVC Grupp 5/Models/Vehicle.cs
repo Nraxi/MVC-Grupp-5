97e0e07f8db8024b4c00e2749793753e027185dd
Eugenia <musat.eugenia@gmail.com>
2024-04-17 15:53:14 +0200

    Add Vehicle model och Type

---
+++ MVC Grupp 5/Models/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MVC_Grupp_5.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        [StringLength(6)]
+        public string RegNr { get; set; } = string.Empty;
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+        public VehicleType VehicleType { get; set; }
+
+    }
+}
---
+++ MVC Grupp 5/Models/VehicleType.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Grupp_5.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Motorcycle,
+        Airplane
+    }
+}
