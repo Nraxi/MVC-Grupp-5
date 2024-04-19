20d255a22a75ee6a577f9638f7153153d0177cdb
Eugenia <musat.eugenia@gmail.com>
2024-04-19 14:27:36 +0200

    Add validation for RegNr and other length limits

--- MVC Grupp 5/Models/Vehicle.cs
+++ MVC Grupp 5/Models/Vehicle.cs
@@ -5,12 +5,18 @@ namespace MVC_Grupp_5.Models
     public class Vehicle
     {
         [Key]
-        [StringLength(6)]
+        [RegularExpression(@"^[^-\s]+$", ErrorMessage = "Registration Number cannot contain spaces or negative values.")]
+        [StringLength(6, ErrorMessage = "Registration Number must be 6 characters long")]
         [Display(Name = "Registration Number")]
+        [Required(ErrorMessage = "Registration Number is required.")]
         public string RegNr { get; set; } = string.Empty;
+        [StringLength(20, ErrorMessage = "Model must have a limit of 20 characters long")]
+
         public string? Model { get; set; }
+        [StringLength(20, ErrorMessage = "Color must have a limit of  20 characters long")]
+
         public string? Color { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Vehicle Type is required.")]
         [Display(Name = "Vehicle Type")]
         public VehicleType VehicleType { get; set; }
         [DataType(DataType.Date)]
