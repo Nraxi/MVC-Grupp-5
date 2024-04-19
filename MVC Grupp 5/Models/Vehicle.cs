829dbde35ce87197b6fce211cf1482dda5dbfe3f
Olle Tengnér <olle.code@hotmail.com>
2024-04-19 21:43:38 +0200

    fix fixes

--- MVC Grupp 5/Models/Vehicle.cs
+++ MVC Grupp 5/Models/Vehicle.cs
@@ -14,7 +14,7 @@ namespace MVC_Grupp_5.Models
         [Display(Name = "Vehicle Type")]
         public VehicleType VehicleType { get; set; }
         [DataType(DataType.Date)]
-        [Display(Name = "Check In Parking Date")]
+        [Display(Name = "Check In / Parking Date")]
         public DateTime CheckInVehicle { get; set; }
 
 
